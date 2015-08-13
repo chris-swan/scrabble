2e135dac9d21a8d0dc4a6c6057122b584a71fad3
Kyle Pratuch & Chris Swan <student@epicodus.com>
2015-08-13 11:34:16 -0700

    debug twig templates

--- app/app.php
+++ app/app.php
@@ -7,7 +7,14 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use ($app) {
-        return $app['twig']->render('scrabble_form.html.twig', array('array' => $output));
+        return $app['twig']->render('scrabble_form.html.twig');
+    });
+
+    $app->get('/score', function() use ($app) {
+        $new = new Scrabble;
+        $output_score = $new->scoreWord($_GET['word']);
+        $word = $_GET['word'];
+        return $app['twig']->render('score.html.twig', array('score' => $output_score, 'word' => $word));
     });
 
     return $app;
