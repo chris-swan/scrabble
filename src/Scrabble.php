6ab363ac74f73ea2fa8dbf14edbd4baf028204d4
Kyle Pratuch & Chris Swan <student@epicodus.com>
2015-08-13 12:06:11 -0700

    Add dictionary to check entered word vs real world

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -16,28 +16,34 @@
 
             $numbers_now = array();
 
-            foreach($split as $letter) {
-                if (in_array($letter, $one_points)) {
-                    $letter = 1;
-                } elseif (in_array($letter, $two_points)) {
-                    $letter = 2;
-                } elseif (in_array($letter, $three_points)) {
-                    $letter = 3;
-                } elseif (in_array($letter, $four_points)) {
-                    $letter = 4;
-                } elseif (in_array($letter, $five_points)) {
-                    $letter = 5;
-                } elseif (in_array($letter, $eight_points)) {
-                    $letter = 8;
-                } elseif (in_array($letter, $ten_points)) {
-                    $letter = 10;
-                } else {
-                    $letter = 0;
+            $dictionary = (file_get_contents('https://docs.oracle.com/javase/tutorial/collections/interfaces/examples/dictionary.txt'));
+            $real_words = explode("\n",$dictionary);
+            if (in_array($lc_word, $real_words)) {
+                foreach($split as $letter) {
+                    if (in_array($letter, $one_points)) {
+                        $letter = 1;
+                    } elseif (in_array($letter, $two_points)) {
+                        $letter = 2;
+                    } elseif (in_array($letter, $three_points)) {
+                        $letter = 3;
+                    } elseif (in_array($letter, $four_points)) {
+                        $letter = 4;
+                    } elseif (in_array($letter, $five_points)) {
+                        $letter = 5;
+                    } elseif (in_array($letter, $eight_points)) {
+                        $letter = 8;
+                    } elseif (in_array($letter, $ten_points)) {
+                        $letter = 10;
+                    } else {
+                        $letter = 0;
+                    }
+                  array_push($numbers_now, $letter);
                 }
-              array_push($numbers_now, $letter);
+            } else {
+                return "That's not a real word, cheater!";
             }
 
-            $output = array_sum($numbers_now);
+            $output = array_sum($numbers_now) . ' points!';
 
             return $output;
 
